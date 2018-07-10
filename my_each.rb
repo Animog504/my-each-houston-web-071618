318098d885ff591cf13c802f26f5d52f9d3a04c6
Scott Ungchusri <animoogle504@gmail.com>
2018-07-10 05:44:39 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -7,7 +7,7 @@ def my_each(collection) # put argument(s) here
        yield(collection[iterator])
        iterator += 1 
     end
-    
+    collection
   else
    # "There is no block given!"
   end
