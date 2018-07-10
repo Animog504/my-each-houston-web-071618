a4d73316ac5354ca66ff01783a4ce816338cb0e4
Scott Ungchusri <animoogle504@gmail.com>
2018-07-10 05:13:22 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -3,7 +3,7 @@ def my_each(collection) # put argument(s) here
   if block_given?
     iterator = 0 
     while iterator < collection.length
-       yield
+       yield(collection[iterator])
        iterator += 1 
     end
   else
