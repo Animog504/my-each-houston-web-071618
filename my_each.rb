b7b56ebd723d19d37ed597cdb875d4fd68e5d411
Scott Ungchusri <animoogle504@gmail.com>
2018-07-10 05:03:27 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  if block_given?
+    iterator = 0 
+    while iterator < collection.length
+       yield
+       iterator += 1 
+    end
+  else
+    "There is no block given!"
+  end
 end
+
+#collection = [1,2,3,4]
+#my_each(collection) do |x|
+#  puts x
+#end
